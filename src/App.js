dcfab7e7ac8d147db49fedeab6539b6e817e7e66
Rghv77 <108543757+Rghv77@users.noreply.github.com>
2023-10-11 22:03:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+//.env is a file name to .gitignore to prevents the file for pushing on github 
+// isse hmari api key safe rhegi , mtlb noone can access our api or noone can see our api_key
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
+  );
+}
+
+export default App;
